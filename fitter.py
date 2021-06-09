7607a7026c3f8534e53f5f20514e9fa3925e9596
Sun Yudong <sunjerry019@gmail.com>
2021-06-09 22:04:50 +0200

    move m_sq specific code to MsqFitter()

--- fitter.py
+++ fitter.py
@@ -5,7 +5,7 @@ import scipy.odr
 
 import fit_functions
 
-class Fitter():
+class ODRFitter():
     """The Fitter class fits the given data using scipy.odr
 
     Parameters
@@ -18,23 +18,21 @@ class Fitter():
         Rank 1, Error in x, should be of the same shape as ``x`` or func(x) --> xerror
     yerror : array_like or function
         Rank 1, Error in y, should be of the same shape as ``y`` or func(y) --> yerror
-    func : function, optional
-        fcn(beta, x) --> y, by default `self.omega_z` (Guassian Beam Profile function)
+    func : function
+        fcn(beta, x) --> y
     
     """
 
-    def __init__(self, x, y, xerror, yerror, func = fit_functions.omega_z):
-        # This is because https://stackoverflow.com/a/41921291
-
+    def __init__(self, x, y, xerror, yerror, func):
         self.model = scipy.odr.Model(func)
 
         self.data = None
-        self.load_data(x, y, xerror, yerror)
+        self.loadData(x, y, xerror, yerror)
 
         self.odr    = None
         self.output = None
 
-    def load_data(self, x, y, xerror, yerror):
+    def loadData(self, x, y, xerror, yerror):
         """Load the data into a data object
 
         Parameters
@@ -67,6 +65,11 @@ class Fitter():
         self.output : Output instance
             This object is also assigned to the attribute .output of Fitter
             https://docs.scipy.org/doc/scipy/reference/generated/scipy.odr.Output.html
+
+            In particular:
+                self.output.res_var = chi_sq_red // https://arxiv.org/abs/1012.3754
+                self.output.beta    = Estimated parameter values
+                self.output.sd_beta = Standard deviations of the estimated parameters
         """
 
         self.odr = scipy.odr.ODR(self.data, self.model, beta0 = initial_params)
@@ -86,7 +89,57 @@ class Fitter():
             self.output.pprint()
         else:
             raise RuntimeWarning(".fit() has not been run. Please run .fit() before printing output")
+
+class MsqFitter(ODRFitter):
+    """Class to fit for an M_Squared using fit_functions.omega_z (Guassian Beam Profile function),
+
+    By default, initial guesses for w_0 and z_0 are 1.
+    Use self.estimateInitialGuesses() to estimate w_0, z_0
+
+    Parameters
+    ----------
+    x : array_like
+        Rank-1, Independent variable
+    y : array_like
+        Rank-1, Dependent variable, should be of the same shape as ``x``
+    xerror : array_like or function
+        Rank 1, Error in x, should be of the same shape as ``x`` or func(x) --> xerror
+    yerror : array_like or function
+        Rank 1, Error in y, should be of the same shape as ``y`` or func(y) --> yerror
+    wavelength : float_like
+        Wavelength of the laser, to be given manually for fitting
+
+    """
+    def __init__(self, x, y, xerror, yerror, wavelength):        
+        # NOTE: To use ``fit_functions.omega_z`` as a default value in a function: https://stackoverflow.com/a/41921291
+
+        self.wavelength      = wavelength
+        self.initial_guesses = np.array([1  , 1  , wavelength], dtype = np.float64)
+        #                                w_0, z_0, M_sq_lmbda
+
+        super().__init__(x, y, xerror, yerror, fit_functions.omega_z)
+
+    def estimateInitialGuesses(self):
+        """Estimates the initial parameters w_0, z_0 from the data given using the minimum y-value and save it into self.initial_guesses.
+        """
+
+        min_w = np.argmin(self.data.y)
+
+        z_0 = self.data.x[min_w]
+        w_0 = self.data.y[min_w]
+
+        self.initial_guesses[0:2] = [w_0, z_0]
     
+    def fit(self):
+        """Fits using self.initial_guesses and ODRFitter.fit()
+
+        Returns
+        -------
+        self.output : Output instance
+            See ODRFitter.fit() for more information
+
+        """
+        return super().fit(initial_params = self.initial_guesses)
 
 if __name__ == "__main__":
     import code; code.interact(local=locals())
--- tests/fit_sample_data.test.py
+++ tests/fit_sample_data.test.py
@@ -15,19 +15,14 @@ print("X-Axis")
 x = data["position[mm]"] / np.power(10, 3)
 y = data["diam_x[um]"]   / (np.power(10, 6) * 2)
 
-f = fitter.Fitter(
-	x      = x, 
-	y      = y, 
-	xerror = lambda x: 0.01*x, 
-	yerror = lambda y: 0.01*y
+f = fitter.MsqFitter(
+	x          = x, 
+	y          = y, 
+	xerror     = lambda x: 0.01*x, 
+	yerror     = lambda y: 0.01*y,
+	wavelength = 2300e-9
 )
 
-w_0   = 142e-6
-z_0   = 205e-3
-m_sq  = 1
-lmbda = 2300e-9
-
-m_sq_lmbda = m_sq * lmbda
-
-f.fit(initial_params = np.array([w_0, z_0, m_sq_lmbda]))
+f.estimateInitialGuesses()
+f.fit()
 f.printOutput()
