3c36f3f0604411414e6048f2d4c2a7f577be0050
Yudong Sun <Yudong.Sun@physik.uni-muenchen.de>
2021-06-25 13:07:19 +0200

    setup on gar-ex-laap06

--- github_setup.sh
+++ github_setup.sh
@@ -2,7 +2,7 @@ git remote add origin git@github.com:sunjerry019/nanosquared.git
 git remote add github git@github.com:sunjerry019/nanosquared.git
 git remote add physik git@gitlab.physik.uni-muenchen.de:Yudong.Sun/nanosquared.git
 git remote set-url --add --push origin git@github.com:sunjerry019/nanosquared.git
-git remote set-url --add --push origin git@github.com:sunjerry019/nanosquared.git
+git remote set-url --add --push origin git@gitlab.physik.uni-muenchen.de:Yudong.Sun/nanosquared.git
 
 # To sync
 git fetch --all
