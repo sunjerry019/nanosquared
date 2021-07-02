f91b396116c51e3adcad0868b12c61353e1cf1cb
Yudong Sun <Yudong.Sun@physik.uni-muenchen.de>
2021-07-02 14:34:33 +0200

    Catastropic Error with pywin32 and comtypes

--- cameras/camera.py
+++ cameras/camera.py
@@ -3,5 +3,11 @@
 """File provides the class camera that all camera types should inherit"""
 
 class Camera():
-	def __init__(self):
-		pass
+    def __init__(self):
+        pass
+    
+    def __enter__(self):
+        return self
+
+    def __exit__(self, e_type, e_val, traceback):
+        pass
--- cameras/wincamd.py
+++ cameras/wincamd.py
@@ -1,12 +1,34 @@
 #!/usr/bin/env python3
 
 import os,sys
+
+import comtypes
 base_dir = os.path.dirname(os.path.realpath(__file__))
 root_dir = os.path.abspath(os.path.join(base_dir, ".."))
 sys.path.insert(0, root_dir)
 
 import cameras.camera as cam
 
+import comtypes
+import comtypes.client as cc
+
+import logging
+import gc
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
 class WinCamD(cam.Camera):
 	def __init__(self):
-		pass
+		self.dataControl = cc.CreateObject("DATARAYOCX.GetDataCtrl.1")
+		self.dataControl.StartDriver()
+	
+	def __exit__(self, e_type, e_val, traceback):
+		del self.dataControl
+		gc.collect()
+		return super().__exit__(e_type, e_val, traceback)
+
+if __name__ == '__main__':
+    with WinCamD() as w:
+        print("with WinCamD as w")
+        import code; code.interact(local=locals())
