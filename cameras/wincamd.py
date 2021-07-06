483cd2ccdb4218b931d5c755b66a5d1d64c6a6cb
Yudong Sun <Yudong.Sun@physik.uni-muenchen.de>
2021-07-06 19:15:52 +0200

    get Data from WinCam

--- cameras/wincamd.py
+++ cameras/wincamd.py
@@ -11,18 +11,72 @@ import cameras.camera as cam
 import logging
 from PyQt5 import QtWidgets, QAxContainer
 
+import numpy as np
+from collections import namedtuple
+
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
 class WinCamD(cam.Camera):
 	def __init__(self, *args, **kwargs):
+		super().__init__(*args, **kwargs)
+
 		self.dummyapp = QtWidgets.QApplication([''])
 		self.dataCtrl = QAxContainer.QAxWidget("DATARAYOCX.GetDataCtrl.1")
 				
 		assert self.dataCtrl.dynamicCall("StartDriver") # Returns True if successful
 
-		super().__init__(*args, **kwargs)
+		axis = {
+			"x" : QAxContainer.QAxWidget("DATARAYOCX.ProfilesCtrl.1"),
+			"y" : QAxContainer.QAxWidget("DATARAYOCX.ProfilesCtrl.1")
+		}
+		self.axis = namedtuple("Axis", axis.keys())(*axis.values())
+		self.axis.x.setProperty("ProfileID", 22)
+		self.axis.y.setProperty("ProfileID", 23)
 
+	def getAxisProfile(self, axis):
+		"""Get the profile in one `axis` if the camera is running.
+
+		Parameters
+		----------
+		axis : str
+			May take values 'x' or 'y'.
+
+		Returns
+		-------
+		ret : Union[array, None]
+			If the given `axis` is not 'x' or 'y', then `None`
+
+		"""
+		if not self.apertureOpen:
+			return None
+
+		data = {
+			"x": self.axis.x.dynamicCall("GetProfileDataAsVariant"),
+			"y": self.axis.y.dynamicCall("GetProfileDataAsVariant")
+		}
+		return np.array(data.get(axis, None))
+	
+	def getWinCamData(self):
+		"""Gets the WinCam Data as a numpy_array if the camera is running, else `None`
+
+		Returns
+		-------
+		data : Union[array_like, None]
+			returns the data, or None if the camera is not running.
+
+		"""
+		if not self.apertureOpen:
+			return None
+
+		vert = self.dataCtrl.dynamicCall("GetVerticalPixels")
+		hori = self.dataCtrl.dynamicCall("GetHorizontalPixels")
+		return np.array(self.dataCtrl.dynamicCall("GetWinCamDataAsVariant")).reshape((vert, hori))
+
+	def getCameraIndex(self):
+		return self.dataCtrl.dynamicCall("GetCameraIndex")
+
+	# Basic Functions
 	def startDevice(self):
 		"""Starts the Camera capturing
 
@@ -48,13 +102,13 @@ class WinCamD(cam.Camera):
 		"""
 
 		ret = self.dataCtrl.dynamicCall("StopDevice")
-		if ret:
-			self.apertureOpen = False
+		# Seems to always return false
+		self.apertureOpen = False
 		
 		return ret
 	
 	def __exit__(self, e_type, e_val, traceback):
-		if self.deviceOn:
+		if self.apertureOpen:
 			self.stopDevice()
 
 		return super().__exit__(e_type, e_val, traceback)
