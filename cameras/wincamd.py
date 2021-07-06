ec585a62bbe68d6e007c88edc891f0f8b94ded69
Yudong Sun <Yudong.Sun@physik.uni-muenchen.de>
2021-07-06 15:03:41 +0200

    Code can now talk to OCX

--- cameras/wincamd.py
+++ cameras/wincamd.py
@@ -19,9 +19,46 @@ logger.setLevel(logging.DEBUG)
 class WinCamD(cam.Camera):
 	def __init__(self):
 		self.dummyapp = QtWidgets.QApplication([''])
-		self.wrapper  = QAxContainer.QAxWidget("DATARAYOCX.GetDataCtrl.1")
+		self.dataCtrl = QAxContainer.QAxWidget("DATARAYOCX.GetDataCtrl.1")
+				
+		assert self.dataCtrl.dynamicCall("StartDriver") # Returns True if successful
+
+		self.deviceOn = False
+
+	def startDevice(self):
+		"""Starts the Camera capturing
+
+		Returns
+		-------
+		ret : bool
+			True if the device is successfully started
+
+		"""
+		ret = self.dataCtrl.dynamicCall("StartDevice")
+		self.deviceOn = ret
+		
+		return ret
+	
+	def stopDevice(self):
+		"""Stops the Camera from capturing
+
+		Returns
+		-------
+		ret : bool
+			True if the device is successfully stopped
+
+		"""
+
+		ret = self.dataCtrl.dynamicCall("StopDevice")
+		if ret:
+			self.deviceOn = False
+		
+		return ret
 	
 	def __exit__(self, e_type, e_val, traceback):
+		if self.deviceOn:
+			self.stopDevice()
+
 		return super().__exit__(e_type, e_val, traceback)
 
 if __name__ == '__main__':
