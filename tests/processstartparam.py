56355ff18cb9c3aaa9b2593fd5cd87c79ed932d2
Sun Yudong <sunjerry019@gmail.com>
2021-08-23 20:08:37 +0200

    MPI Process start vals

--- tests/processstartparam.py
+++ tests/processstartparam.py
@@ -15,30 +15,102 @@ comm = MPI.COMM_WORLD
 size = comm.Get_size()
 rank = comm.Get_rank()
 
+NUM_X = 10
+NUM_Y = 10
+
+total = NUM_X * NUM_Y
+chunksize = int(np.floor(total / size))
+
+eachNode    = [chunksize] * size  
+eachNode[0] = total - (size - 1) * chunksize
+# the number of datapoints each node should get
+# The main node gets more
+
+sendbuf = None
+recvbuf = None
+chunks  = None
+
 if rank == 0:
 	diode_data_fast = pd.read_csv('../data/diode/fast_axis.txt', delimiter = '; ', engine='python', decimal=",")	
 	x = diode_data_fast["position[mm]"]
 	y = diode_data_fast["diam_y[um]"] / 2
+
+	min_w = np.argmin(y)
+
+	z_0 = x[min_w]
+	w_0 = y[min_w]
+	
+	# Variation
+	z_0_values = z_0 + np.linspace(-5, 5, endpoint=True, num = NUM_X, dtype = 'float64')
+	w_0_values = w_0 + np.linspace(-5, 5, endpoint=True, num = NUM_Y, dtype = 'float64')
+	
+	# https://stackoverflow.com/a/11144716
+	# Cartesian Product
+	allcombi = np.transpose([np.tile(z_0_values, len(w_0_values)), np.repeat(w_0_values, len(z_0_values))])
+	
+	# Split into chunks for scattering
+	chunks = np.array(np.split(allcombi, np.cumsum(eachNode))[:-1])
+	print(chunks.shape)
+	# has length = size
+	# Remove the last one because it is an empty array
 else:
 	x = np.empty(19, dtype='float64')
 	y = np.empty(19, dtype='float64')
 
+	z_0s = np.empty(eachNode[rank], dtype='float64')
+	w_0s = np.empty(eachNode[rank], dtype='float64')
+
 x = comm.bcast(x, root=0)
 y = comm.bcast(y, root=0)
 
-print(f"{rank}: {x[0]}, {y[0]}")
+# https://mpi4py.readthedocs.io/en/stable/tutorial.html > Scattering numpy array
+mychunk = np.empty((eachNode[rank], 2) , dtype='float64')
+comm.Scatter(chunks, mychunk, root=0)
 
-# f = fitting.fitter.MsqFitter(
-# 	x              = diode_data_fast["position[mm]"], 
-# 	y              = diode_data_fast["diam_y[um]"] / 2, 
-# 	xerror         = 0.5,
-# 	yerror         = 1,
-# 	wavelength     = 1650,
-# 	wavelength_err = 0
-# )
+print(f"{rank}: shape = {mychunk.shape}")
 
-# f.setInitialGuesses(rank, rank)
-# print(f"Received {f.initial_guesses}")
+f = fitting.fitter.MsqFitter(
+	x              = x, 
+	y              = y, 
+	xerror         = 0.5,
+	yerror         = 1,
+	wavelength     = 1650,
+	wavelength_err = 0
+)
+print(f"{rank}: Processing start")
 
-# f.estimateInitialGuesses()
-# f.printOutput()
+results = []
+
+for (z_0, w_0) in mychunk:
+	f.setInitialGuesses(w_0 = w_0, z_0 = z_0)
+	f.fit()
+
+	if (f.output.info >= 4):
+		result = {
+			"init": (str(z_0), str(w_0)),
+			"m2"  : ['0', '0'], 
+			"beta": [str(i) for i in f.output.beta]
+		}
+	else:
+		result = {
+			"init": (str(z_0), str(w_0)),
+			"m2"  : [str(i) for i in f.m_squared], 
+			"beta": [str(i) for i in f.output.beta]
+		}
+
+	results.append(result)
+
+comm.Barrier()
+print(f"{rank}: Processing end")
+
+results = comm.gather(results, root=0)
+
+if rank == 0:
+	results = [item for sublist in results for item in sublist]
+	with open("output.ignore.out", 'w') as f:
+		f.write("# init_z\tinit_w\tm2\tdm2\tbeta\n")
+		for res in results:
+			init = '\t'.join(res['init'])
+			m2   = '\t'.join(res['m2'])
+			beta = '\t'.join(res['beta'])
+			f.write("{}\t{}\t{}\n".format(init, m2, beta))
