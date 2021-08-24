3246474ab8247ceea34d039a3e51441ae0845da6
Yudong Sun <Yudong.Sun@physik.uni-muenchen.de>
2021-08-24 09:30:33 +0200

    adjust to make it 20

--- tests/slurm_proc.sh
+++ tests/slurm_proc.sh
@@ -2,13 +2,14 @@
 #
 #SBATCH --job-name=procStartParams
 #SBATCH --comment="Process Start Params of fitting"
-#SBATCH --ntasks=1
-#SBATCH --time=12:00:00
+#SBATCH --ntasks=20
+#SBATCH --time=24:00:00
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=Yudong.Sun@physik.uni-muenchen.de
 #SBATCH --chdir=/home/y/Yudong.Sun/attoworld/nanosquared/tests
 #SBATCH --output=/home/y/Yudong.Sun/attoworld/slurm/slurm.%j.%N.out
 #SBATCH --error=/home/y/Yudong.Sun/attoworld/slurm/slurm.%j.%N.err.out
 
+source /etc/profile.d/modules.sh
 module load openmpi
-srun mpiexec -n 1 python3 processstartparam.py
+srun mpiexec -n 20 python3 processstartparam.py
