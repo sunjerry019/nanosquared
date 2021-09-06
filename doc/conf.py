a1e1f0c8eca37bd8034127aa711808ecc0a0fcc3
Yudong Sun <Yudong.Sun@physik.uni-muenchen.de>
2021-09-06 21:20:34 +0200

    reorganize src

--- doc/conf.py
+++ doc/conf.py
@@ -12,12 +12,12 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../'))
+sys.path.insert(0, os.path.abspath(os.path.join('..', 'src')))
 
 
 # -- Project information -----------------------------------------------------
 
-project = 'Nanoscan M-Squared Automation'
+project = 'M-Squared Automation'
 copyright = '2021, Yudong Sun'
 author = 'Yudong Sun'
 
