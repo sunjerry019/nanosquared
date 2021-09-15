eb146d0632465a0fcb30f7cdcf7d8eec47be4e7f
Yudong Sun <Yudong.Sun@physik.uni-muenchen.de>
2021-09-15 14:47:45 +0200

    debugging the lack of data

--- src/cameras/combi_wincamd_test.py
+++ src/cameras/combi_wincamd_test.py
@@ -14,9 +14,14 @@ from cameras.wincamd import WinCamD
 from cameras.camera  import Camera
 import common.helpers as h
 
+from stage.controller import Controller, GSC01
+
 class A(h.LoggerMixIn):
     def __init__(self) -> None:
+        self.controller = GSC01(devMode = False)
         self.cam = WinCamD(devMode = False)
         assert isinstance(self.cam, Camera), f"Camera ({self.cam}) is not recognized"
+        print(self.cam.getAxis_avg_D4Sigma('x'))
+
 
 x = A()
