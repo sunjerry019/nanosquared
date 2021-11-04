864cf5a78f410c60131520a642b79e37cf465dc3
Yudong Sun <Yudong.Sun@physik.uni-muenchen.de>
2021-11-04 17:19:30 +0100

    Does not crash but 'iDispatch does not provide interface' error

--- src/cameras/nanoscan.py
+++ src/cameras/nanoscan.py
@@ -7,7 +7,6 @@ import os,sys
 
 import ctypes
 
-import win32com
 
 base_dir = os.path.dirname(os.path.realpath(__file__))
 root_dir = os.path.abspath(os.path.join(base_dir, ".."))
@@ -21,9 +20,6 @@ from PyQt5 import QtWidgets, QAxContainer, QtCore
 import numpy as np
 from collections import namedtuple
 
-# import nanoscan_activex as NSAx
-import win32com.client as wc
-
 class NanoScan(cam.Camera):
 	"""Provides interface to the NanoScan 2s Pyro/9/5"""
 
@@ -32,25 +28,18 @@ class NanoScan(cam.Camera):
 
 		self.devMode = devMode
 
-		# self.dummyapp = QtWidgets.QApplication([''])
+		self.dummyapp = QtWidgets.QApplication([''])
 
 		# Early Binding: NanoScanII.INanoScanII
 		# We have to use Late Binding
-		# self.NS = QAxContainer.QAxWidget("photon-nanoscan")  # {FAAD0D22-C718-459A-81CA-268CCF188807}
-		# print(self.NS.dynamicCall("NsAsGetDeviceID()"))
+		self.NS = QAxContainer.QAxWidget("photon-nanoscan")  # {FAAD0D22-C718-459A-81CA-268CCF188807}
 
-		# ui = QtCore.QMetaType(36) # ushort https://doc.qt.io/qt-5/qmetatype.html
-		ui = QtCore.QMetaType(31) # voidstar https://doc.qt.io/qt-5/qmetatype.html
-		# ui = np.array([-1], dtype=np.int16)
-		# ui = ctypes.c_short(-1)
+		wahr = QtCore.QVariant(True)
+		self.NS.setProperty("NsAsShowWindow(int)", wahr)
 
-		x = self.NS.dynamicCall("NsAsGetNumDevices", ui)
-		print(x, ui)
-
-		# x = 10
-		# self.NS = wc.Dispatch("photon-nanoscan")
-		# self.NS.NsAsGetNumDevices(ui)
-		# print(ui)
+		ui = QtCore.QVariant(-1)
+		x = self.NS.dynamicCall("NsAsGetNumDevices(short&)", ui) # https://stackoverflow.com/a/25378588
+		print(ui.value())
 				
 
 	def __exit__(self, e_type, e_val, traceback):
