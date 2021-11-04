dec5005d34aba41bfd1a66d292461b1ec6cc8ee6
Yudong Sun <Yudong.Sun@physik.uni-muenchen.de>
2021-11-04 14:13:33 +0100

    backup initial attempts that did not work

--- src/cameras/nanoscan.py
+++ src/cameras/nanoscan.py
@@ -5,6 +5,10 @@
 
 import os,sys
 
+import ctypes
+
+import win32com
+
 base_dir = os.path.dirname(os.path.realpath(__file__))
 root_dir = os.path.abspath(os.path.join(base_dir, ".."))
 sys.path.insert(0, root_dir)
@@ -17,6 +21,9 @@ from PyQt5 import QtWidgets, QAxContainer, QtCore
 import numpy as np
 from collections import namedtuple
 
+# import nanoscan_activex as NSAx
+import win32com.client as wc
+
 class NanoScan(cam.Camera):
 	"""Provides interface to the NanoScan 2s Pyro/9/5"""
 
@@ -25,16 +32,25 @@ class NanoScan(cam.Camera):
 
 		self.devMode = devMode
 
-		self.dummyapp = QtWidgets.QApplication([''])
+		# self.dummyapp = QtWidgets.QApplication([''])
 
 		# Early Binding: NanoScanII.INanoScanII
 		# We have to use Late Binding
-		self.NS = QAxContainer.QAxWidget("photon-nanoscan")  # {FAAD0D22-C718-459A-81CA-268CCF188807}
+		# self.NS = QAxContainer.QAxWidget("photon-nanoscan")  # {FAAD0D22-C718-459A-81CA-268CCF188807}
 		# print(self.NS.dynamicCall("NsAsGetDeviceID()"))
 
-		ui = QtCore.QMetaType(36) # ushort https://doc.qt.io/qt-5/qmetatype.html
-		x = self.NS.dynamicCall("NsAsGetDeviceID", ui)
+		# ui = QtCore.QMetaType(36) # ushort https://doc.qt.io/qt-5/qmetatype.html
+		ui = QtCore.QMetaType(31) # voidstar https://doc.qt.io/qt-5/qmetatype.html
+		# ui = np.array([-1], dtype=np.int16)
+		# ui = ctypes.c_short(-1)
+
+		x = self.NS.dynamicCall("NsAsGetNumDevices", ui)
 		print(x, ui)
+
+		# x = 10
+		# self.NS = wc.Dispatch("photon-nanoscan")
+		# self.NS.NsAsGetNumDevices(ui)
+		# print(ui)
 				
 
 	def __exit__(self, e_type, e_val, traceback):
