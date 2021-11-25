7dff37b525a6ee792ba39acd215bb21cc6d438c0
Yudong Sun <Yudong.Sun@physik.uni-muenchen.de>
2021-11-25 13:42:44 +0100

    generation of function mappings; partially done removing pass by references

--- src/cameras/archive/nanoscan_dotnet.py
+++ src/cameras/archive/nanoscan_dotnet.py
@@ -1,7 +1,8 @@
 from msl.loadlib import LoadLibrary
+# run in 32 bit python
 
 # We must publish as netstandard2.0
-net = LoadLibrary("./csharp/NanoScanLibrary/bin/Release/netstandard2.0/NanoScanLibrary.dll", 'net')
+net = LoadLibrary("../csharp/NanoScanLibrary/bin/Release/netstandard2.0/NanoScanLibrary.dll", 'net')
 for item in dir(net.lib):
     if not item.startswith('_'):
         attr = getattr(net.lib, item)
--- src/cameras/csharp/NanoScanLibrary/generate.py
+++ src/cameras/csharp/NanoScanLibrary/generate.py
@@ -1,5 +1,21 @@
+import types
+
+
 with open("Dllimports.cs", 'r') as f:
     for line in f:
         line = line.strip()
         if (line != "") and ("static extern" in line):
-            print(len(line), line)
+            splitted    = line.split()
+
+            type_index  = splitted.index("extern")
+
+            return_type = splitted[type_index + 1]
+            func_name   = splitted[type_index + 2].split("(")[0]
+            temp        = func_name.split("NsInterop")
+            my_func     = temp[1] if temp[1] != "" else temp[0]
+
+            params      = line.split("(")[1].split(")")[0]
+            params_2    = ", ".join([x.split(" ")[-1] for x in params.split(",")])
+
+            ret         = "return " if return_type != 'void' else ""
+            print(f"public {return_type} {my_func} ({params}) {{ {ret}{func_name}({params_2}); }}")
