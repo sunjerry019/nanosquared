13cafd0fc89ae788b6165d11325e9ebb6485187a
Yudong Sun <Yudong.Sun@physik.uni-muenchen.de>
2021-12-02 09:38:01 +0100

    C# example has been implemented

--- src/cameras/nanoscan.py
+++ src/cameras/nanoscan.py
@@ -12,8 +12,10 @@ sys.path.insert(0, root_dir)
 import cameras.camera as cam
 
 import logging
+import time
 
 from msl.loadlib import Client64
+from nanoscan_constants import SelectParameters as NsSP
 
 class NanoScan(cam.Camera):
 	"""Provides interface to the NanoScan 2s Pyro/9/5"""
@@ -23,6 +25,65 @@ class NanoScan(cam.Camera):
 
 		self.devMode = devMode
 		self.NS = NanoScanDLL() # Init and Shutdown is done by the 32-bit server
+
+		assert self.NS.GetNumDevices() > 0, "No devices connected"
+
+		self.daqState = False
+		self.aperture = 0
+		self.roiIndex = 0
+	
+	def SetDAQ(self, state: bool) -> None:
+		"""Sets the DAQ state. Use this instead of directly using `self.NS.SetDataAcquisition`. This helps to keep track of the DAQ State.
+
+		Parameters
+		----------
+		state : bool
+			Sets the Data Acquisition to `state`
+
+		"""
+
+		self.NS.SetDataAcquisition(state)
+		self.daqState = state
+
+	def start(self) -> int:
+		# self.NS.SetShowWindow(True)
+		self.SetDAQ(True)
+		self.waitForData()
+		self.NS.SelectParameters(NsSP.BEAM_WIDTH_D4SIGMA)
+		x = self.NS.GetBeamWidth4Sigma(self.aperture, self.roiIndex)
+		self.SetDAQ(False)
+		return x
+	
+	def waitForData(self) -> bool:
+		"""A valid method of determining whether data has been processed yet is
+		to evaluate whether any Results (Parameters per NS1) have yet been computed.
+		In this example the Centroid position result is used due to its benign
+		nature, i.e. usually enabled and not affected by other settings or results.
+
+		Reference: Program.cs from Automation examples folder from NanoScan
+		"""
+		if not self.daqState:
+			self.log("Start DAQ before waiting for data. Ignoring function call", logging.WARN)
+			return False
+
+		self.NS.SelectParameters(
+			NsSP.BEAM_CENTROID_POS
+		)
+
+		daqState = False
+		centroidValue = 0
+		while not daqState:
+			time.sleep(50e-3)
+			centroidValue = self.NS.GetCentroidPosition(self.aperture, self.roiIndex)
+
+			if (centroidValue > 0):
+				daqState = True
+		
+		return True
+
+	def __exit__(self, e_type, e_val, traceback):
+		self.NS.__exit__(e_type, e_val, traceback)
+		return super(NanoScan, self).__exit__(e_type, e_val, traceback)
 
 class NanoScanDLL(Client64):
 	"""Provides interface to the 32-bit NanoScan C# DLL using msl-loadlib."""
@@ -39,7 +100,8 @@ class NanoScanDLL(Client64):
 		return self
 
 	def __exit__(self, e_type, e_val, traceback):
-		return super().__exit__(e_type, e_val, traceback)
+		return self.ShutdownNS()
+		# return super().__exit__(e_type, e_val, traceback)
 
 if __name__ == '__main__':
     with NanoScan() as n:
