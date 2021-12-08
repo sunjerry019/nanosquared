248edecb785f4d2fc7ffed918c5979d5edc3975a
Yudong Sun <Yudong.Sun@physik.uni-muenchen.de>
2021-12-08 12:06:24 +0100

    Copy the dlls to a local folder to prevent error due to remote sources

--- src/cameras/archive/nanoscan_32_dotnet.py
+++ src/cameras/archive/nanoscan_32_dotnet.py
@@ -1,12 +1,19 @@
 from msl.loadlib import LoadLibrary
 # run in 32 bit python
 
+import clr
+import System
+
 # We must publish as netstandard2.0
-net = LoadLibrary("../csharp/NanoScanLibrary/bin/Release/netstandard2.0/NanoScanLibrary.dll", 'net')
+try:
+    net = LoadLibrary("../csharp/NanoScanLibrary/bin/Release/netstandard2.0/NanoScanLibrary.dll", 'net')
+except System.NotSupportedException as e:
+    net = LoadLibrary("C:/nanosquared_include/NanoScanLibrary.dll", 'net')
+
 for item in dir(net.lib):
     if not item.startswith('_'):
         attr = getattr(net.lib, item)
         print('{} {}'.format(item, type(attr)))
 
 x = net.lib.NanoScanLibrary.NanoScan()
-print(x.InitNs())
+print(x.InitNS())
