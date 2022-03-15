b604d027bc758faeaa0aea41f0d4379377a69be6
Yudong Sun <Yudong.Sun@physik.uni-muenchen.de>
2022-03-15 19:40:40 +0100

    update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="nanosquared",
-    version="0.1.1-alpha",
+    version="0.2",
     author="Yudong Sun",
     author_email="yudong.sun@mpq.mpg.de",
     description="Automated M-Squared Measurement",
