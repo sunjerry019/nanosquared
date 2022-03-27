9a930cb59357b157a8ba586c99bf6f84fd952540
Yudong Sun <Yudong.Sun@physik.uni-muenchen.de>
2022-03-27 14:24:31 +0200

    conda-environment file location fixed

--- anaconda-update.bat
+++ anaconda-update.bat
@@ -2,7 +2,7 @@
 git pull origin master
 
 call %ProgramData%\Anaconda3\Scripts\activate.bat base
-conda env update --name nanosquared --file ../conda-environment.yml --prune
+conda env update --name nanosquared --file ./conda-environment.yml --prune
 conda activate nanosquared
 pip install .
 echo Press any key to exit . . .
