c0003f58d6f7564d01d8585a50bd11e97de54b64
Yudong Sun <Yudong.Sun@physik.uni-muenchen.de>
2022-04-04 10:28:28 +0200

    manually consider first and last data point

--- tests/outlier/spike.py
+++ tests/outlier/spike.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import scipy.signal
 
-data = pd.read_csv("dataset_41.dat", sep=",", header = 0)
+data = pd.read_csv("dataset_40.dat", sep=",", header = 0)
 
 x = data['x'].to_numpy()
 horizontal = np.arange(len(x))
@@ -48,6 +48,24 @@ def remove_spikes(arr: np.ndarray, threshold: float) -> np.ndarray:
         
         i += 1
 
+    # manually check first and last points
+    l    = len(arr_rem)
+    mask = np.ones(l, dtype=bool)
+
+    d = arr_rem[0] - arr_rem[1]
+    if np.abs(d) > threshold:
+        mask[0] = False
+
+    d = arr_rem[-1] - arr_rem[-2]
+    if np.abs(d) > threshold:
+        mask[-1] = False
+
+    horizontal_rem = horizontal_rem[mask] 
+    arr_rem        = arr_rem[mask]
+
+    plt.plot(horizontal_rem, arr_rem, "o", label = f"Cycle Final")
+
+
     plt.ylabel('Beam Diam [um]')
     plt.xlabel('Time [sec]')
     plt.legend()
@@ -57,7 +75,7 @@ def remove_spikes(arr: np.ndarray, threshold: float) -> np.ndarray:
     return arr_rem
 
 def get_hist(x):
-    hist, bin_edges = np.histogram(x, bins = 30)
+    hist, bin_edges = np.histogram(x, bins = 10)
     conv_kernel     = np.array([0.5, 0.5])
     bin_centers = np.convolve(bin_edges, conv_kernel, mode = "valid")
 
@@ -75,3 +93,5 @@ def get_hist(x):
 
 rs = remove_spikes(x, 0.2 * np.average(x))
 
+print(np.average(x), np.std(x))
+print(np.average(rs), np.std(rs))
