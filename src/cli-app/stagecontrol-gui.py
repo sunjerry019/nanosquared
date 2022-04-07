e3aee276320c8a8a8dc92b08efe7c894dc09028c
Yudong Sun <Yudong.Sun@physik.uni-muenchen.de>
2022-04-07 09:47:39 +0200

    Add velocity

--- src/cli-app/stagecontrol-gui.py
+++ src/cli-app/stagecontrol-gui.py
@@ -77,27 +77,14 @@ class Stgctrl(QtWidgets.QWidget):
         self._numsamples.setAlignment(QtCore.Qt.AlignCenter)
         self._numsamples.setValidator(QtGui.QIntValidator(1,10000))
 
-        # _velocity_label = QtWidgets.QLabel("Velocity ({}m/s)".format(self.MICROSYMBOL))
-        # _velocity_label.setAlignment(QtCore.Qt.AlignRight)
-        # _step_size_label = QtWidgets.QLabel("Step size ({}m)".format(self.MICROSYMBOL))
 
-        # self._SL_velocity = QtWidgets.QLineEdit()
-        # self._SL_velocity.setText('100')
-        # self._SL_velocity.setValidator(QtGui.QDoubleValidator(0,10000, 12))
-        # # _velocity.setFont(QtGui.QFont("Arial",20))
+        _velocity_label = QtWidgets.QLabel("Jog Speed (pulses/s)")
+        _velocity_label.setAlignment(QtCore.Qt.AlignCenter | QtCore.Qt.AlignBottom)
 
-        # self._SL_step_size = QtWidgets.QLineEdit()
-        # self._SL_step_size.setText('10')
-        # self._SL_step_size.setValidator(QtGui.QDoubleValidator(0.5,10000, 12))
-        # # _step_size.setFont(QtGui.QFont("Arial",20))
-
-        # _SL_settings = QtWidgets.QWidget()
-        # _SL_settings_layout = QtWidgets.QVBoxLayout()
-        # self._SL_invertx_checkbox = QtWidgets.QCheckBox("Invert Horizontal")
-        # self._SL_inverty_checkbox = QtWidgets.QCheckBox("Invert Vertical")
-        # _SL_settings_layout.addWidget(self._SL_invertx_checkbox)
-        # _SL_settings_layout.addWidget(self._SL_inverty_checkbox)
-        # _SL_settings.setLayout(_SL_settings_layout)
+        self._velocity = QtWidgets.QLineEdit()
+        self._velocity.setText('500')
+        self._velocity.setValidator(QtGui.QIntValidator(100,20000))
+        self._velocity.setAlignment(QtCore.Qt.AlignCenter)
 
         # Create the layout with the child elements
         _stage_layout = QtWidgets.QGridLayout()
@@ -107,25 +94,18 @@ class Stgctrl(QtWidgets.QWidget):
 
         # currx, y label
         _stage_layout.addWidget(_lcd_label, 0, 0, 1, 2)
-        _stage_layout.addWidget(self._lcd, 1, 0, 2, 2)
+        _stage_layout.addWidget(self._lcd, 1, 0, 4, 2)
 
-        _stage_layout.addWidget(self._leftArrow, 3, 0, 1, 1)
-        _stage_layout.addWidget(self._rightArrow, 3, 1, 1, 1)
-        _stage_layout.addWidget(self._homeBtn, 4, 0, 1, 2)
+        _stage_layout.addWidget(self._leftArrow, 5, 0, 1, 1)
+        _stage_layout.addWidget(self._rightArrow, 5, 1, 1, 1)
+        _stage_layout.addWidget(self._homeBtn, 6, 0, 1, 2)
 
-        _stage_layout.addWidget(_numsamples_label, 1, 3, 1, 1)
-        _stage_layout.addWidget(self._numsamples, 2, 3, 1, 1)
-        _stage_layout.addWidget(self._d4sigmaBtn, 3, 3, 2, 1)
+        _stage_layout.addWidget(_velocity_label, 1, 3, 1, 1)
+        _stage_layout.addWidget(self._velocity, 2, 3, 1, 1)
 
-        # _stage_layout.addWidget(_velocity_label, 2, 0)
-        # _stage_layout.addWidget(self._SL_velocity, 2, 1, 1, 2)
-        # _stage_layout.addWidget(self._SL_step_size, 2, 3, 1, 2)
-        # _stage_layout.addWidget(_step_size_label, 2, 5)
-        
-
-        # _stage_layout.addWidget(_SL_settings, 4, 4, 1, 2)
-
-        
+        _stage_layout.addWidget(_numsamples_label, 3, 3, 1, 1)
+        _stage_layout.addWidget(self._numsamples, 4, 3, 1, 1)
+        _stage_layout.addWidget(self._d4sigmaBtn, 5, 3, 2, 1)
 
         return _stage_layout
 
@@ -137,52 +117,8 @@ class Stgctrl(QtWidgets.QWidget):
 
         moveToCentre(self)
 
-        # self.textbox = QLabel(self)
-        # self.textbox.setText("HELLO WORLD\n")
-        # self.setStyleSheet("QLabel {font-weight: bold; font-size: 18pt; font-family: Roboto, 'Segoe UI'; }")
-        # self.textbox.setMaximumHeight(100)
-        # # self.textbox.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Minimum)
-
-        # button = QPushButton('LAUNCH\nSEQUENCE', self)
-        # button.setToolTip('A single button. Click it maybe?')
-        # button.setStyleSheet("QPushButton {background-color: red; color: black; font-weight: bold; font-size: 20pt; font-family: Roboto, 'Segoe UI';}")
-        # button.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
-        # button.setMaximumHeight(300)
-        # button.setMaximumWidth(350)
-        # button.clicked.connect(self.on_click)
-
-        # self._layout.addWidget(self.textbox, 0, 0)
-
-        # # https://stackoverflow.com/a/25515321/3211506
-        # self.horizontal_wrapper = QWidget()
-        # self.horizontal_wrapper_layout = QHBoxLayout()
-        # self.horizontal_wrapper_layout.setContentsMargins(0, 0, 0, 0)
-        # self.horizontal_wrapper_layout.addStretch(1)
-        # self.horizontal_wrapper_layout.addWidget(button)
-        # self.horizontal_wrapper_layout.setStretchFactor(button, 3)
-        # self.horizontal_wrapper_layout.addStretch(1)
-        # self.horizontal_wrapper.setLayout(self.horizontal_wrapper_layout)
-
-        # self._layout.addWidget(self.horizontal_wrapper, 1, 0)
-
-
-        # # We still set this as this is not default behaviour on Windows
-        # self._layout.setAlignment(self.textbox, Qt.AlignCenter)
-        # # self._layout.setAlignment(button, Qt.AlignCenter)
-
         self._layout = self.create_stage()
         self.setLayout(self._layout)
-
-    # @pyqtSlot()
-    # def on_click(self):
-    #     if self.isOpen:
-    #         self.shutter.close()
-    #         self.isOpen = False
-    #         self.textbox.setText("You closed the shutter\nGood job!!")
-    #     else:
-    #         self.shutter.open()
-    #         self.isOpen = True
-    #         self.textbox.setText("You opened the shutter\nWE'RE DOOMED!!")
 
 def main():
     # https://stackoverflow.com/a/1857/3211506
