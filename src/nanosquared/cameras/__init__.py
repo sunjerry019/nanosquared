a3d007f0e442da8eb98a49bd988c4862b511efea
Yudong Sun <Yudong.Sun@physik.uni-muenchen.de>
2022-04-14 12:13:05 +0200

    Skip import if bitness is 32

--- src/nanosquared/cameras/__init__.py
+++ src/nanosquared/cameras/__init__.py
@@ -1,10 +1,12 @@
-try:
+import struct
+
+# https://stackoverflow.com/a/1405971/3211506
+bitness = 8 * struct.calcsize("P")
+
+if bitness > 32:
     from . import all_constants
     from . import camera
     from . import nanoscan_constants
     from . import nanoscan
     from . import wincamd_constants
     from . import wincamd
-except ModuleNotFoundError:
-    # Probably got called from msl-loadlib/Server32
-    pass
